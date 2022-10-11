74598b2c89a34067edda52cbd8bcb016c0b1e411
Regina Ochoa Gispert <0244214@up.edu.mx>
2022-10-11 14:58:04 -0500

    avances

--- P2-09-10-22-Node.js/Exercise-3/index.js
+++ P2-09-10-22-Node.js/Exercise-3/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-fs.writeFile ('helloworld.txt', 'Hello world!', (err) => {
-    if (err) return console.log(err);
-    console.log('Hello world > helloworld.txt');
+fs.writeFile('helloworld.txt', 'Hello World!', (err) => {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.txt');
 });
--- P2-29-09-22-Express-test1/server.js
+++ P2-29-09-22-Express-test1/server.js
@@ -1,16 +1,20 @@
 const express = require("express"); //get the modle into the screen, its the input
 const app = express(); //with this 2 lines we have a server
+app.engine("ejs", require("ejs").renderFile); //11.2k (gzipped: 4.2k)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("public")); //inside of public we can add everything we want
+app.set("view engine", "ejs");
 
 app.get("/",(req, res) =>{ // /url/npoint //ipoint is a url adress //to configurate it:
     //res.send("Hello world");
-    res.sendFile(__dirname + "/public/html/index.html"); //dirname: refers to the current location of the file
+    //res.sendFile(__dirname + "/public/html/index.html"); //dirname: refers to the current location of the file
+    res.render("home");
 }); //(request=INPUT an object that contains all the info that I´m receiveing and response=OUTPUT no create new npoints)
 app.get("/students",(req, res) =>{
     var name = req.query.name;
-    res.send("Hello " + name);
+    /* res.send("Hello " + name);*/
+    res.render("test", {name: name});
 });
 app.post("/students",(req, res) =>{
     var name = req.body.name;
