490178404f70b3e8c4f5fce6743acf08a4c5db63
Regina Ochoa Gispert <0244214@up.edu.mx>
2022-10-15 11:09:16 -0500

    avancessss

---
+++ P2-14-10-22-Mailchimp-Tutorial/signup-news/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+const https = require("https");
+const apiKey = "bd6d2f4652bc70e6a7358a383befcb9b-us9";
+const list_id = "c893142775";
+const url = "https://us9.api.mailchimp.com/3.0/lists/"+list_id;
+const options = {
+    method: "POST",
+    auth: "0244214@up.edu.mx:" + apiKey
+};
+
+app.get("/",(req, res) =>{ 
+    res.sendFile(__dirname + "/public/html/signup.html"); //dirname: refers to the current location of the file
+});
+
+app.post("/registry",(req, res) =>{
+    var fName = req.body.fName;
+    var lName = req.body.lName;
+    var email = req.body.email;
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fName,
+                LNAME: lName
+            }
+        }]
+    };
+    var mailRequest = https.request(url, options, (response) => {
+        if(response.statusCode === 200) {
+            response.on("data", (data) => {
+                var jsonResp = JSON.parse(data);
+                if(jsonResp["error_count"] === 0) {
+                    res.sendFile(__dirname +"/public/html/success.html");
+                } else {
+                    res.sendFile(__dirname +"/public/html/failure.html");
+                    console.log(jsonResp.errors[0]["error_code"]);
+                    console.log(jsonResp.errors[0]["error"]);
+                }
+            }).on("error", (e) => {
+                res.sendFile(__dirname +"/public/html/failure.html");
+            });
+        } else {
+            res.sendFile(__dirname +"/public/html/failure.html");
+        }
+    });
+    var jsonData = JSON.stringify(data);
+    mailRequest.write(jsonData);
+    mailRequest.end();
+});
+
+app.get("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/success", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
--- P2-29-09-22-Express-test1/server.js
+++ P2-29-09-22-Express-test1/server.js
@@ -6,15 +6,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); //inside of public we can add everything we want
 app.set("view engine", "ejs");
 
+const welcomeMsg = "Welcome to my blog."
+const longContent = "Lacus vel facilis volutpat est velit egestas dui id omare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit"
+let posts = [];
+
 app.get("/",(req, res) =>{ // /url/npoint //ipoint is a url adress //to configurate it:
     //res.send("Hello world");
     //res.sendFile(__dirname + "/public/html/index.html"); //dirname: refers to the current location of the file
-    res.render("home");
+    posts.push({
+        title: "My entry blog", 
+        content: longContent
+    });
+    res.render("home", { startingContent: welcomeMsg, posts: posts });
 }); //(request=INPUT an object that contains all the info that I´m receiveing and response=OUTPUT no create new npoints)
+
+app.get("/posts/:blogTitle", (req, res) => { //shows the content that I am clicking on
+    res.render("post",{ post: posts[0] }); //post is the name of my template, parameters I need to show
+});
+
 app.get("/students",(req, res) =>{
     var name = req.query.name;
     /* res.send("Hello " + name);*/
-    res.render("test", {name: name});
+    res.render("test", { name: name }); //Jason object=python dictionary with key and the value
 });
 app.post("/students",(req, res) =>{
     var name = req.body.name;
